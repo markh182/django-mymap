ce28f54591e57e5138979268d64407c601d9f1bc
Mark Harris <markthebassist@hotmail.com>
2020-03-30 23:25:35 +0200

    CSS fixes and DB Query with empty DB

--- coronamap/views.py
+++ coronamap/views.py
@@ -13,28 +13,49 @@ class CovidMap(TemplateView):
     template_name = "index.html"
 
     def get_context_data(self, **kwargs):
+
+        try:
+            object = Covid19Info.objects.last()
+        except Covid19Info.DoesNotExist:
+            object = None
+        if object:
+            last_date = Covid19Info.objects.latest('date')
+            date = last_date.date
+        else:
+            date = today
+
         last_date = Covid19Info.objects.latest('date')
         context = super(CovidMap, self).get_context_data(**kwargs)
-        context['confirmed'] = Covid19Info.objects.order_by('-confirmed').filter(date=last_date.date)
-        context['confirmed_number'] = Covid19Info.objects.filter(date=last_date.date).aggregate(Sum('confirmed'))
-        context['deaths'] = Covid19Info.objects.order_by('-deaths').filter(date=last_date.date)
-        context['deaths_number'] = Covid19Info.objects.filter(date=last_date.date).aggregate(Sum('deaths'))
-        context['recovered'] = Covid19Info.objects.order_by('-recovered').filter(date=last_date.date)
-        context['recovered_number'] = Covid19Info.objects.filter(date=last_date.date).aggregate(Sum('recovered'))
+        context['confirmed'] = Covid19Info.objects.order_by('-confirmed').filter(date=date)
+        context['confirmed_number'] = Covid19Info.objects.filter(date=date).aggregate(Sum('confirmed'))
+        context['deaths'] = Covid19Info.objects.order_by('-deaths').filter(date=date)
+        context['deaths_number'] = Covid19Info.objects.filter(date=date).aggregate(Sum('deaths'))
+        context['recovered'] = Covid19Info.objects.order_by('-recovered').filter(date=date)
+        context['recovered_number'] = Covid19Info.objects.filter(date=date).aggregate(Sum('recovered'))
         return context
 
 class CovidMapByCountry(TemplateView):
     template_name = "indexbycountry.html"
 
     def get_context_data(self, **kwargs):
-        last_date = Covid19InfoByCountry.objects.latest('date')
+
+        try:
+            object = Covid19InfoByCountry.objects.last()
+        except Covid19InfoByCountry.DoesNotExist:
+            object = None
+        if object:
+            last_date = Covid19InfoByCountry.objects.latest('date')
+            date = last_date.date
+        else:
+            date = today
+
         context = super(CovidMapByCountry, self).get_context_data(**kwargs)
-        context['confirmed'] = Covid19InfoByCountry.objects.order_by('-confirmed').filter(date=last_date.date).filter(country=self.kwargs.get('country'))
-        context['confirmed_number'] = Covid19InfoByCountry.objects.filter(date=last_date.date).filter(country=self.kwargs.get('country')).aggregate(Sum('confirmed'))
-        context['deaths'] = Covid19InfoByCountry.objects.order_by('-deaths').filter(date=last_date.date).filter(country=self.kwargs.get('country'))
-        context['deaths_number'] = Covid19InfoByCountry.objects.filter(date=last_date.date).filter(country=self.kwargs.get('country')).aggregate(Sum('deaths'))
-        context['diff'] = Covid19InfoByCountry.objects.order_by('-difference').filter(date=last_date.date).filter(country=self.kwargs.get('country'))
-        context['diff_number'] = Covid19InfoByCountry.objects.filter(date=last_date.date).filter(country=self.kwargs.get('country')).aggregate(Sum('difference'))
+        context['confirmed'] = Covid19InfoByCountry.objects.order_by('-confirmed').filter(date=date).filter(country=self.kwargs.get('country'))
+        context['confirmed_number'] = Covid19InfoByCountry.objects.filter(date=date).filter(country=self.kwargs.get('country')).aggregate(Sum('confirmed'))
+        context['deaths'] = Covid19InfoByCountry.objects.order_by('-deaths').filter(date=date).filter(country=self.kwargs.get('country'))
+        context['deaths_number'] = Covid19InfoByCountry.objects.filter(date=date).filter(country=self.kwargs.get('country')).aggregate(Sum('deaths'))
+        context['diff'] = Covid19InfoByCountry.objects.order_by('-difference').filter(date=date).filter(country=self.kwargs.get('country'))
+        context['diff_number'] = Covid19InfoByCountry.objects.filter(date=date).filter(country=self.kwargs.get('country')).aggregate(Sum('difference'))
         context['centralCountry'] =  Covid19Info.objects.all().filter(country=self.kwargs.get('country')).filter(province='')[:1]
         context['country'] = self.kwargs.get('country')
         return context
@@ -52,8 +73,18 @@ class MapLayer(GeoJSONLayerView):
     use_natural_keys = False # use_natural_keys : serialize natural keys instead of primary keys (default: False)
 
     def get_queryset(self):
-        last_date = Covid19Info.objects.latest('date')
-        queryset = Covid19Info.objects.filter(date=last_date.date)
+
+        try:
+            object = Covid19Info.objects.last()
+        except Covid19Info.DoesNotExist:
+            object = None
+        if object:
+            last_date = Covid19Info.objects.latest('date')
+            date = last_date.date
+        else:
+            date = today
+
+        queryset = Covid19Info.objects.filter(date=date)
         return queryset
 
 class MapLayerByCountry(GeoJSONLayerView):
@@ -68,6 +99,16 @@ class MapLayerByCountry(GeoJSONLayerView):
     use_natural_keys = False # use_natural_keys : serialize natural keys instead of primary keys (default: False)
 
     def get_queryset(self):
-        last_date = Covid19InfoByCountry.objects.latest('date')
-        queryset = Covid19InfoByCountry.objects.filter(country=self.kwargs.get('country')).filter(date=last_date.date)
+
+        try:
+            object = Covid19InfoByCountry.objects.last()
+        except Covid19InfoByCountry.DoesNotExist:
+            object = None
+        if object:
+            last_date = Covid19InfoByCountry.objects.latest('date')
+            date = last_date.date
+        else:
+            date = today
+
+        queryset = Covid19InfoByCountry.objects.filter(country=self.kwargs.get('country')).filter(date=date)
         return queryset
